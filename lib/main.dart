7c544b9644846c48d22d77ac16b76fe7a88cc32e
Nimesh-Giri <nimeshgiri871@gmail.com>
2024-12-06 15:57:40 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+
+
+  double? bmi;
+
+  void calculate() {
+
+    double height = double.tryParse(heightController.text) ?? 0;
+    double weight = double.tryParse(weightController.text) ?? 0;
+
+    if (height > 0 && weight > 0) {
+      setState(() {
+        bmi = weight / (height * height);
+      });
+    } else {
+      setState(() {
+        bmi = null;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueAccent,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('BMI Calculator'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: TextField(
+                    controller: heightController,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Height (m)',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: TextField(
+                    controller: weightController,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 20, color: Colors.black),
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Weight (kg)',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: calculate,
+                child: const Text('Calculate'),
+              ),
+              const SizedBox(height: 20),
+              if (bmi != null)
+                Text(
+                  "Your BMI is: ${bmi!.toStringAsFixed(2)}",
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
